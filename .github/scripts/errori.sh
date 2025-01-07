7c5e9cb77b106d51ce6f15880db9bef48892fe97
Pedro Leoni <pedroleoni2001@gmail.com>
2025-01-07 21:04:15 +0100

    boh

--- .github/scripts/errori.sh
+++ .github/scripts/errori.sh
@@ -8,7 +8,7 @@ do
     clean_file=$(cat $file | sed -e '/\\begin{lstlisting}/, /\\end{lstlisting}/d' -e 's/\\lstinline|[^|]*|//g' -e 's/\\lstinline+[^+]*+//g')
 
     # Esecuzione aspell sul file ripulito
-    errors=$(echo "$clean_file" |  | aspell list --mode=tex --lang=it --home-dir=. --ignore-case --encoding=utf-8 --add-tex-command='label p' --add-tex-command='hyperref op' --add-tex-command='texttt p' --add-tex-command='dirtree p' --add-tex-command='href pp' --add-tex-command='ref p'| aspell list --lang=en --encoding=utf-8)
+    errors=$(echo "$clean_file" | aspell list --mode=tex --lang=it --home-dir=. --ignore-case --encoding=utf-8 --add-tex-command='label p' --add-tex-command='hyperref op' --add-tex-command='texttt p' --add-tex-command='dirtree p' --add-tex-command='href pp' --add-tex-command='ref p'| aspell list --lang=en --encoding=utf-8)
 
     for error in $errors
     do
