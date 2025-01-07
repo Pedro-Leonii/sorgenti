a738b14a9f0f17d45d120b5a0cfee67d5cc86518
Pedro Leoni <pedroleoni2001@gmail.com>
2025-01-07 19:48:31 +0100

    contr grammaticale finito?

--- .github/scripts/errori.sh
+++ .github/scripts/errori.sh
@@ -8,26 +8,20 @@ do
     clean_file=$(cat $file | sed -e '/\\begin{lstlisting}/, /\\end{lstlisting}/d' -e 's/\\lstinline|[^|]*|//g' -e 's/\\lstinline+[^+]*+//g')
 
     # Esecuzione aspell sul file ripulito
-    prob_errors=$(echo "$clean_file" | aspell list --mode=tex --lang=it --encoding=utf-8 --add-tex-command='label p' --add-tex-command='hyperref op' --add-tex-command='texttt p' --add-tex-command='dirtree p' --add-tex-command='href pp' --add-tex-command='ref p'| aspell list --lang=en --encoding=utf-8)
+    errors=$(echo "$clean_file" |  | aspell list --mode=tex --lang=it --home-dir=. --ignore-case --encoding=utf-8 --add-tex-command='label p' --add-tex-command='hyperref op' --add-tex-command='texttt p' --add-tex-command='dirtree p' --add-tex-command='href pp' --add-tex-command='ref p'| aspell list --lang=en --encoding=utf-8)
 
-    positions_of_error=()
-    for prob_error in $prob_errors
+    for error in $errors
     do
-        # Se il possibile errore non è contenuto nel dizionario allora è un errore
-        if [[ -z $(grep -wi "$prob_error" "dizionario.txt") ]]
-        then
-            # Il file contiene almeno un errore
-            #echo "## Il file $(basename $file) contiene i seguenti errori:" >> errori.md
+        # Il file contiene almeno un errore
+        #echo "## Il file $(basename $file) contiene i seguenti errori:" >> errori.md
 
-            # Calcola il numero di riga in cui l'errore compare
-            error_positions=$(sed -n '/'"$prob_error"'/=' "$file")
+        # Calcola il numero di riga in cui l'errore compare
+        error_positions=$(sed -n '/'"$error"'/=' "$file")
 
-            for pos in $error_positions
-            do
-                # Per ogni errore aggiunge al file errori.md un link all'errore
-                echo "- [Rilevato errore $prob_error alla riga $pos]($file/#L$pos)" >> errori.md
-            done
-
-        fi
+        for pos in $error_positions
+        do
+            # Per ogni errore aggiunge al file errori.md un link all'errore
+            echo "- [Rilevato errore $error alla riga $pos]($file/#L$pos)" >> errori.md
+        done
     done
 done
